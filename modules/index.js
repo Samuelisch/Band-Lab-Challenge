eec4a409221f9abb136fc5eab06afb30a13f8395
Samuelisch <samuelchanwk@hotmail.com>
2021-10-01 22:18:23 +0800

    refactored index.js file to change pages on button click

--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,5 @@
 import postPage from './post-list.js'
+import audioPage from './audio-sample.js'
 
 // selectors for page buttons
 const postsBtn = document.getElementById('post-page');
@@ -6,4 +7,15 @@ const audioBtn = document.getElementById('audio-page');
 
 const content = document.getElementById('content');
 
-content.appendChild(postPage)
+const changeToPostsPage = () => {
+  content.innerHTML = '';
+  content.appendChild(postPage);
+}
+
+const changeToAudioPage = () => {
+  content.innerHTML = '';
+  content.appendChild(audioPage);
+}
+
+postsBtn.addEventListener('click', changeToPostsPage);
+audioBtn.addEventListener('click', changeToAudioPage);
